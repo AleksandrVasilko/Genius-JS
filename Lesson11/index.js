8bb9f4556285568792d7a83182118eb43bdbf595
AleksandrVasilko <avasilko7@gmail.com>
2024-11-30 23:31:25 +0200

    add liberys

--- Lesson11/index.js
+++ Lesson11/index.js
@@ -1,4 +1,22 @@
 import { name } from './module';
 import './style.css';
+import isEmail from 'validator/lib/isEmail';
+import isURL from 'validator/lib/isURL';
+import { toDate } from 'validator';
+import moment from 'moment/moment';
 
-console.log("Your name: " + name);
+//console.log("Your name: " + name);
+
+var validator = require('validator');
+
+validator.isEmail('foo@bar.com');
+
+//console.log(validator.isEmail('foo@bar.com')); 
+
+console.log(isEmail('foo@bar.com')); 
+console.log(isURL("dsdsds"));
+console.log(isURL("https://testprovider.com/ua/catalog/js-online-ua"));
+console.log(toDate("12.12.2000"));
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+console.log(moment().format('MMMM Do YY'));
