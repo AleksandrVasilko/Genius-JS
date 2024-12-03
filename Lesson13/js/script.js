d7429b35a6a43847a84ecc7be65df933527a5086
AleksandrVasilko <avasilko7@gmail.com>
2024-12-03 23:28:21 +0200

    edit code

--- Lesson13/js/script.js
+++ Lesson13/js/script.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+const btn = document.querySelector('.js-get-data');
+
+btn.addEventListener("click", getURL);
+
+function getURL() { 
+    return fetch("https://api.monobank.ua/bank/currency")
+} 
